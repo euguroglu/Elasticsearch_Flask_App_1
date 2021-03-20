7c23af1ae1612c51438878927f333a9169ee39a6
euguroglu <e.uguroglu@yahoo.com>
2021-03-20 22:09:54 +0300

    elasticsearch simple search application

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template, request
+from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
+es = Elasticsearch(HOST="http://localhost", PORT=9200)
+es = Elasticsearch()
+
+@app.route('/',methods=["GET","POST"])
+def index():
+
+    #q = request.args.get("q") # For get method
+    q = request.form.get("q")
+    if q is not None:
+
+        res = es.search(index="access-logs-2020-02", body={"query": {"prefix" : {"client.geo.city_name":q}}})
+        return render_template("index.html", q=q, res=res)
 
 
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
